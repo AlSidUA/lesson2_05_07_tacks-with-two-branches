b6610dd31bf94d212e655a2a0b7afe07fe2bbd91
Alexandr Sidash <alexsidashwg@gmail.com>
2023-07-14 10:20:38 +0200

    two commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import naturalNumber from './tacks/tacks1';
+
 
 function App() {
   return (
     <div className="App">
+      {
+        `The result of solving problem No. 1: ${naturalNumber(10)}`
+      }
+      
       
     </div>
   );
---
+++ src/tacks/tacks1.js
@@ -0,0 +1,40 @@
+//Задача № 1
+/*Якщо ми перерахуємо всі натуральні числа нижче
+10, кратні 3 або 5, то отримаємо 3, 5, 6, 9. Сума цих кратних
+дорівнює 23. Завершіть розв`язання так, щоб воно повернуло
+суму всіх кратних 3 або 5 нижче переданого числа. Крім того,
+якщо число від`ємне, поверніть 0. (Якщо число кратне 3 і 5,
+порахуйте його один раз*/
+
+//Варіант 1
+const naturalNumber = (limit) => {
+    let Result = 0;
+        for (let i = 1; i < limit; i++) {
+            if (i%3===0||i%5===0) {
+                Result+=i
+                }else if(limit<0){
+                return 0
+            }
+        }
+    return Result
+}
+
+export default naturalNumber;
+
+    
+
+  //Варіант 2
+
+/*const naturalNum = (limit) => {
+    let Arr = [];
+        for (let i = 1; i < limit; i++) {
+            if (i%3===0||i%5===0) {
+                Arr.push(i);
+            }else if(limit<0){
+                return 0
+            }
+        }
+    return Arr.reduce((acc, curr)=>acc+curr, 0);
+}
+
+//export default naturalNum;*/
