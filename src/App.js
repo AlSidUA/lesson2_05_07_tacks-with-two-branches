3ac64a454f230e0557f988dadce366b2cac80da5
Alexandr Sidash <alexsidashwg@gmail.com>
2023-07-14 17:36:43 +0200

    two commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import naturalNumber from './tacks/tacks1';
 
 function App() {
   return (
     <div className="App">
-      
+      {
+        naturalNumber(10)
+      }
     </div>
   );
 }
